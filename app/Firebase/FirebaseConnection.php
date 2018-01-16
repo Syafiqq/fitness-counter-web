6e2f93b6caf11f479fb0c9025208cbc5b32648d1
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-16 21:13:20 +0700

    Initial Commit of Firebase Connection

---
+++ app/Firebase/FirebaseConnection.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This <fitness-counter.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 16 January 2018, 9:02 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Firebase;
+
+
+class FirebaseConnection
+{
+    /**
+     * FirebaseConnection constructor.
+     */
+    public function __construct()
+    {
+    }
+}
+
+?>
