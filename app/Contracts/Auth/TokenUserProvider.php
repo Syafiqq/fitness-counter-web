7b66157690dc857c36355fc74bdde9c7dbdc20df
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-18 06:49:28 +0700

    Add TokenUserProvider

---
+++ app/Contracts/Auth/TokenUserProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This <fitness-counter.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 17 January 2018, 9:29 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Contracts\Auth;
+
+
+use Illuminate\Contracts\Auth\UserProvider;
+
+interface TokenUserProvider extends UserProvider
+{
+}
+
+?>
