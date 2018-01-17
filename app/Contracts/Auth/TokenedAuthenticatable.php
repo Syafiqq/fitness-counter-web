5f21c3b582c6e1af54f4a9cec77420aba967999c
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-18 06:49:13 +0700

    Add Tokened Authenticable

---
+++ app/Contracts/Auth/TokenedAuthenticatable.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This <fitness-counter.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 17 January 2018, 9:39 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Contracts\Auth;
+
+
+use Illuminate\Contracts\Auth\Authenticatable;
+
+interface TokenedAuthenticatable extends Authenticatable
+{
+}
+
+?>
