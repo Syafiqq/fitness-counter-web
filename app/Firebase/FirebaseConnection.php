f87cea9ca36a0a36415f53b727b76e268c323a79
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-17 20:03:16 +0700

    Add Firebase Connection

--- app/Firebase/FirebaseConnection.php
+++ app/Firebase/FirebaseConnection.php
@@ -10,13 +10,37 @@
 namespace App\Firebase;
 
 
+use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
 class FirebaseConnection
 {
+    /**
+     * @var \Kreait\Firebase
+     */
+    private $connection;
+    private $serviceAccount;
+    private $apiKey;
+
     /**
      * FirebaseConnection constructor.
      */
     public function __construct()
     {
+        $this->serviceAccount = ServiceAccount::fromJsonFile(resource_path() . env('FIREBASE_SERVICE', '/assets/sdk/fitness-counter-6a479f0be813.json'));
+        $this->apiKey         = env('FIREBASE_API_KEY', 'AIzaSyD_xXi_xZo25ASGgFODWv9av5lLLPHRWeg');
+        $this->connection     = (new Factory)
+            ->withServiceAccountAndApiKey($this->serviceAccount, $this->apiKey)
+            ->create();
+    }
+
+    /**
+     * @return \Kreait\Firebase
+     */
+    public function getConnection(): Firebase
+    {
+        return $this->connection;
     }
 }
 
