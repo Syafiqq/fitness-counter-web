e93f57e7a7cbf26e7cd020b505ddf68d46a6756b
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-20 12:24:31 +0700

    Finishing firebase user provider

--- app/Firebase/FirebaseUserProvider.php
+++ app/Firebase/FirebaseUserProvider.php
@@ -79,7 +79,7 @@ class FirebaseUserProvider implements TokenUserProvider
      */
     public function retrieveByCredentials(array $credentials)
     {
-        if (empty($credentials) || (count($credentials) === 1 && array_key_exists('password', $credentials)))
+        if (empty($credentials) || (count($credentials) === 2 && array_key_exists('password', $credentials) && array_key_exists('role', $credentials)))
         {
             return null;
         }
