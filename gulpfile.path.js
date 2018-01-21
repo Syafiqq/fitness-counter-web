cca6a76a326cf2bdf04b9ff5e7d9551a3709aad1
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-21 16:24:53 +0700

    Register vue

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -11,7 +11,8 @@ module.exports = {
             (negated ? '!' : '') + './node_modules/fastclick/lib/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/html5shiv/dist/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/respond.js/dest/**' + (ext === null ? '' : '/' + ext),
-            (negated ? '!' : '') + './node_modules/firebase/**' + (ext === null ? '' : '/' + ext)
+            (negated ? '!' : '') + './node_modules/firebase/**' + (ext === null ? '' : '/' + ext),
+            (negated ? '!' : '') + './node_modules/vue/**' + (ext === null ? '' : '/' + ext)
         ];
     }
 };
