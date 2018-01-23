970e01f9b6575e9030d0e76918e719c7c2ff6fe5
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-24 05:11:35 +0700

    Add Organizer dashboard

---
+++ app/Http/Controllers/Organizer/Dashboard.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This <fitness-counter.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 21 January 2018, 9:39 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Http\Controllers\Organizer;
+
+
+use App\Http\Controllers\Controller;
+
+class Dashboard extends Controller
+{
+    /**
+     * Dashboard constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getHome()
+    {
+        return 'Ini Organizer';
+    }
+}
+
+?>
