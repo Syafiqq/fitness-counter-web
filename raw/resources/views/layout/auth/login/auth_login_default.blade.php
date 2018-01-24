b8899ed2d936812aaccc9416fdc5835a87f01704
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-24 21:21:47 +0700

    Add support ui for auth login

--- raw/resources/views/layout/auth/login/auth_login_default.blade.php
+++ raw/resources/views/layout/auth/login/auth_login_default.blade.php
@@ -1,38 +1,35 @@
+@extends('root.root-theme-default')
 <?php
 /** @var \Collective\Html\FormBuilder $form */
 $form = \Collective\Html\FormFacade::getFacadeRoot();
 ?>
-        <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@section('head-title')
     <title>Login</title>
+@endsection
+
+@section('head-description')
+    <meta name="description" content="Login">
+@endsection
+
+@section('body-content')
+    @parent
+    {!! $form->open(['route' => 'auth.login.post', 'method' => 'post']) !!}
+    {!! nl2br(PHP_EOL) !!}
+    {!! $form->email('email', 'syafiq.rezpector@gmail.com', ['placeholder' => 'Email', 'required'=> true]) !!}
+    {!! nl2br(PHP_EOL) !!}
+    {!! $form->input('password', 'password', 'password', ['value' => 'password','placeholder' => 'Password', 'required'=> true]) !!}
+    {!! nl2br(PHP_EOL) !!}
+    {!! $form->button('Submit', ['type' => 'Submit']) !!}
+    {!! $form->close() !!}
+@endsection
+
+@section('head-css-post')
+    @parent
     <link rel="stylesheet" href="{{asset('/css/layout/auth/login/auth_login_default.min.css')}}">
-</head>
-<body>
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong>
-        There were some problems with your input.
-        <br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-{!! $form->open(['route' => 'auth.login.post', 'method' => 'post']) !!}
-{!! nl2br(PHP_EOL) !!}
-{!! $form->email('email', 'syafiq.rezpector@gmail.com', ['placeholder' => 'Email', 'required'=> true]) !!}
-{!! nl2br(PHP_EOL) !!}
-{!! $form->input('password', 'password', 'password', ['value' => 'password','placeholder' => 'Password', 'required'=> true]) !!}
-{!! nl2br(PHP_EOL) !!}
-{!! $form->button('Submit', ['type' => 'Submit']) !!}
-{!! $form->close() !!}
-<script src="{{asset('js/layout/auth/login/auth_login_default.min.js')}}"></script>
-</body>
-</html>
+@endsection
+
+@section('body-js-lower-post')
+    @parent
+    <script type="text/javascript" src="{{asset('js/layout/auth/login/auth_login_default.min.js')}}"></script>
+@endsection
+
