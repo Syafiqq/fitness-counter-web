a0de9c105c3b9ab0e3448db4aa2671369d2f535c
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-24 21:11:27 +0700

    Extend default theme root

---
+++ raw/resources/views/layout/organizer/dashboard/home/organizer_dashboard_home_default.blade.php
@@ -0,0 +1,23 @@
+@extends('root.root-theme-default')
+@section('head-title')
+    <title>Dashboard</title>
+@endsection
+
+@section('head-description')
+    <meta name="description" content="Dashboard">
+@endsection
+
+@section('body-content')
+    @parent
+
+@endsection
+
+@section('head-css-post')
+    @parent
+    <link rel="stylesheet" href="{{asset('/css/layout/organizer/dashboard/home/organizer_dashboard_home_default.min.css')}}">
+@endsection
+
+@section('body-js-lower-post')
+    @parent
+    <script type="text/javascript" src="{{asset('/js/layout/organizer/dashboard/home/organizer_dashboard_home_default.min.js')}}"></script>
+@endsection
