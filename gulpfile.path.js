73a836bb2c9570199e958750f5b102f41246d8b0
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-24 20:23:07 +0700

    Add Toastr

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -13,6 +13,7 @@ module.exports = {
             (negated ? '!' : '') + './node_modules/respond.js/dest/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/firebase/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/vue/**' + (ext === null ? '' : '/' + ext)
+            (negated ? '!' : '') + './node_modules/toastr/build/**' + (ext === null ? '' : '/' + ext)
         ];
     }
 };
