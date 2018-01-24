95a38306386d72d7f374ec1f355d15cc49ab3d63
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-24 21:22:07 +0700

    Add csrf token

--- raw/resources/views/root/root-boilerplate.blade.php
+++ raw/resources/views/root/root-boilerplate.blade.php
@@ -2,7 +2,8 @@
 
 @section('head-property')
     {{-- Tell the browser to be responsive to screen width --}}
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- Favicon--}}
     <link rel="manifest" href="{{asset('/manifest.min.json')}}">
     <link rel="apple-touch-icon" href="{{asset('/icon.png')}}">
