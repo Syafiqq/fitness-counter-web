3a526cd7db4d919c12167794e1dab447460280ea
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-24 21:41:26 +0700

    Add Logout Featur

--- raw/resources/views/root/root-authenticated-theme-default.blade.php
+++ raw/resources/views/root/root-authenticated-theme-default.blade.php
@@ -1,5 +1,9 @@
 @extends('root.root-theme-default')
-
 @section('body-content')
     @parent
+    @if(\Illuminate\Support\Facades\Auth::check())
+        {!! link_to_route('logout', 'Logout') !!}
+    @endif
+
+    <hr>
 @endsection
