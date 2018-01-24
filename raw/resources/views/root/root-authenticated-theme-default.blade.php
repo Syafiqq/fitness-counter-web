201fc06d1000b9b1cae83d57d6be768b559699a3
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-25 06:49:08 +0700

    Add role changer

--- raw/resources/views/root/root-authenticated-theme-default.blade.php
+++ raw/resources/views/root/root-authenticated-theme-default.blade.php
@@ -2,8 +2,22 @@
 @section('body-content')
     @parent
     @if(\Illuminate\Support\Facades\Auth::check())
-        {!! link_to_route('logout', 'Logout') !!}
+        <?php
+        $roles = \Illuminate\Support\Facades\App::call(\App\Helper\UserHelper::class . "::getUserRole", [\Illuminate\Support\Facades\Auth::user()]);
+        foreach ($roles as $krole => &$role)
+        {
+            $role = ucfirst($krole);
+        }
+        ?>
+        {!! \Collective\Html\FormFacade::select('role', $roles, \Illuminate\Support\Facades\Auth::user()->getRole(), ['id' => 'role-changer']) !!}
+        {!! link_to_route('logout', 'Logout', [], ['style'=> 'margin-left:10px;']) !!}
     @endif
 
     <hr>
 @endsection
+
+@section('body-js-lower-post')
+    @parent
+    <script type="text/javascript" src="{{asset('/js/common/authenticated_theme_default.min.js')}}"></script>
+@endsection
+
