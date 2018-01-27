e6a763150fa1d97f981b93fc1089f7834020cd5d
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-27 20:35:12 +0700

    Remove jquery Remove toastr Remove NProgress

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -1,16 +1,13 @@
 module.exports = {
     assetsVendorResource: function (negated, ext) {
         return [
-            (negated ? '!' : '') + './node_modules/jquery/dist/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/html5-boilerplate/dist/css/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/html5-boilerplate/dist/js/**' + (ext === null ? '' : '/' + ext),
-            (negated ? '!' : '') + './node_modules/nprogress/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/fastclick/lib/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/html5shiv/dist/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/respond.js/dest/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/firebase/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/vue/**' + (ext === null ? '' : '/' + ext),
-            (negated ? '!' : '') + './node_modules/toastr/build/**' + (ext === null ? '' : '/' + ext)
         ];
     }
 };
