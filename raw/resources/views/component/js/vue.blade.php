6c93bfc45c4e8b66283ffc3dd4f1f8f2ce175273
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-27 20:17:04 +0700

    Change to original vue path

--- raw/resources/views/component/js/vue.blade.php
+++ raw/resources/views/component/js/vue.blade.php
@@ -1,3 +1,3 @@
-<script type="text/javascript" src="{{asset('/js/app.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('/vendor/vue/dist/vue.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('/js/model/firebase/PathMapper.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('/js/model/firebase/DataMapper.min.js')}}"></script>
