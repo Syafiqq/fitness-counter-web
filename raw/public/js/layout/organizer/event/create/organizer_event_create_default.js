2c0a1f01529b88d7a3972c5f0dd0e59091362049
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-27 07:35:44 +0700

    Add additional component

--- raw/public/js/layout/organizer/event/create/organizer_event_create_default.js
+++ raw/public/js/layout/organizer/event/create/organizer_event_create_default.js
@@ -3,7 +3,16 @@
         var app = new Vue({
             el: '#app',
             data: {
-                is_logged_out: true
+                is_logged_out: true,
+                f_event: '',
+                f_slug: ''
+            },
+            methods: {
+                createEvent: function () {
+                    console.log(this.f_event, this.f_slug);
+                    this.is_logged_out = true;
+                    NProgress.start();
+                }
             }
         });
         firebase.auth().onAuthStateChanged(function (user) {
