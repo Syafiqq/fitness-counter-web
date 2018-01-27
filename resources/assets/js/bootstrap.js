31f41832f97fd453dec37aa0f5a3e8a8bab06272
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-27 20:14:24 +0700

    Add nprogress script

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,4 +1,3 @@
-
 window._ = require('lodash');
 
 /**
@@ -7,9 +6,13 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
+try
+{
     window.$ = window.jQuery = require('jquery');
-} catch (e) {}
+    window.NProgress = require('nprogress');
+} catch (e)
+{
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -29,9 +32,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
-if (token) {
+if (token)
+{
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
+} else
+{
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
