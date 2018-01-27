a14e7d7c008b455bfe8ada71322d9f3a3929b988
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-27 16:14:34 +0700

    Register path mapper and data mapper

--- raw/resources/views/component/js/vue.blade.php
+++ raw/resources/views/component/js/vue.blade.php
@@ -1,1 +1,3 @@
 <script type="text/javascript" src="{{asset('/vendor/vue/dist/vue.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('/js/model/firebase/PathMapper.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('/js/model/firebase/DataMapper.min.js')}}"></script>
