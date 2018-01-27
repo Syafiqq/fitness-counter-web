a40448c373077a2891f8a67b33d926b8dc107b55
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-27 16:14:15 +0700

    Add Path Mapper

---
+++ raw/public/js/model/firebase/PathMapper.js
@@ -0,0 +1,13 @@
+/**
+ * This <fitness-counter.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 27 January 2018, 3:12 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+var PathMapper =
+        {
+            USERS: 'users',
+            GROUPS: 'groups',
+            USERS_GROUPS: 'users_groups',
+        };
