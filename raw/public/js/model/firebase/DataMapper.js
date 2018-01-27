4567be385ea36a28b2638c412804bd9c03a388e9
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-27 16:14:23 +0700

    Add Data Mapper

---
+++ raw/public/js/model/firebase/DataMapper.js
@@ -0,0 +1,8 @@
+/**
+ * This <fitness-counter.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 27 January 2018, 3:16 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+var DataMapper = {};
