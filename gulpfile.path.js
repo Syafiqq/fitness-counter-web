d818bc20458d1eb7ec90424a52076cfb48128a1f
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-27 19:46:30 +0700

    Remove ionicons path

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -5,7 +5,6 @@ module.exports = {
             (negated ? '!' : '') + './node_modules/html5-boilerplate/dist/css/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/html5-boilerplate/dist/js/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/nprogress/**' + (ext === null ? '' : '/' + ext),
-            (negated ? '!' : '') + './node_modules/ionicons/dist/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/fastclick/lib/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/html5shiv/dist/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/respond.js/dest/**' + (ext === null ? '' : '/' + ext),
