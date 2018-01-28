fba01828512e34a2d68d142c226758f7c5221c71
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-28 08:18:30 +0700

    Remove vue path

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -6,7 +6,6 @@ module.exports = {
             (negated ? '!' : '') + './node_modules/html5shiv/dist/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/respond.js/dest/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/firebase/**' + (ext === null ? '' : '/' + ext),
-            (negated ? '!' : '') + './node_modules/vue/**' + (ext === null ? '' : '/' + ext),
         ];
     }
 };
