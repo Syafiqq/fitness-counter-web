45beafc514f21410fdcd39ceef5f13746d703f99
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-28 07:06:37 +0700

    Re-enable html5shiv

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -3,6 +3,7 @@ module.exports = {
         return [
             (negated ? '!' : '') + './node_modules/html5-boilerplate/dist/css/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/html5-boilerplate/dist/js/**' + (ext === null ? '' : '/' + ext),
+            (negated ? '!' : '') + './node_modules/html5shiv/dist/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/respond.js/dest/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/firebase/**' + (ext === null ? '' : '/' + ext),
             (negated ? '!' : '') + './node_modules/vue/**' + (ext === null ? '' : '/' + ext),
