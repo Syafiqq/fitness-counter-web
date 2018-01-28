582a565ea76b2624fa09edab87eb679adcb4475d
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-28 19:20:12 +0700

    Finishing Adding Event Logic

--- raw/public/js/layout/organizer/dashboard/home/organizer_dashboard_home_default.js
+++ raw/public/js/layout/organizer/dashboard/home/organizer_dashboard_home_default.js
@@ -3,16 +3,49 @@
         var app = new Vue({
             el: '#app',
             data: {
-                is_logged_out: true,
+                is_process: true,
                 f_event: '',
                 f_slug: '',
             },
             methods: {
                 eventFormCommit: function () {
-                    console.log(this.f_event, this.f_slug);
                     NProgress.configure({parent: '.sweet-modal', showSpinner: false});
                     NProgress.start();
-                    this.is_logged_out = true;
+                    app.is_process = true;
+
+                    var event    = PojsoMapper.Event(app.f_event, app.f_slug);
+                    var computed = {};
+                    var mapping  = DataMapper.Event(
+                        firebase.auth().currentUser.uid,
+                        firebase.database().ref().child(DataMapper.Event(firebase.auth().currentUser.uid)['events']).push().key);
+                    $.each(mapping, function (key, value) {
+                        switch (key)
+                        {
+                            case 'events' :
+                            {
+                                computed[value]        = Object.assign({}, event);
+                                computed[value]['uid'] = firebase.auth().currentUser.uid;
+                            }
+                                break;
+                            default :
+                            {
+                                computed[value] = Object.assign({}, event);
+                            }
+                                break;
+                        }
+                    });
+
+                    return firebase.database().ref().update(computed, function (error) {
+                        if (error !== undefined)
+                        {
+                            DoNotify(['Pembuatan Event berhasil'])
+                        } else
+                        {
+                            DoNotify([error])
+                        }
+                        NProgress.done();
+                        app.is_process = false;
+                    });
                 },
                 eventFormOpen: function () {
                     this.$refs.modal.open();
@@ -23,7 +56,7 @@
             if (user)
             {
                 console.log(user.email);
-                app.is_logged_out = false;
+                app.is_process = false;
             } else
             {
                 // User is signed out.
