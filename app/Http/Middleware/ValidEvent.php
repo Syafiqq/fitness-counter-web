7ea0d90de928b3a39828ed42d155c69eb4b2b626
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-31 06:57:28 +0700

    Finishing event validity

--- app/Http/Middleware/ValidEvent.php
+++ app/Http/Middleware/ValidEvent.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Middleware;
 
+use App\Firebase\DataMapper;
+use App\Firebase\FirebaseConnection;
+use App\Model\FirebaseUser;
 use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 
 class ValidEvent
 {
@@ -15,6 +20,17 @@ class ValidEvent
      */
     public function handle($request, Closure $next)
     {
+        /** @var FirebaseConnection $connection */
+        $connection = App::make(\App\Firebase\FirebaseConnection::class);
+        $event      = $request->route('event') ?: sha1(ValidEvent::class);
+        /** @var FirebaseUser $user */
+        $user  = Auth::user();
+        $valid = $connection->getConnection()->getDatabase()->getReference(DataMapper::event($user->getUid(), $event)[0])->getValue() !== null;
+        if (!$valid)
+        {
+            return redirect()->back()->with('cbk_msg', ['notify' => ["Event Tidak Valid"]]);
+        }
+
         return $next($request);
     }
 }
