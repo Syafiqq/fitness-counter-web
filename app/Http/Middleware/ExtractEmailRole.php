bb253c3062cf20d5bf32ed1410fb82044d28de82
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-04 17:52:25 +0700

    Finishing ExtractEmailRole Logic

--- app/Http/Middleware/ExtractEmailRole.php
+++ app/Http/Middleware/ExtractEmailRole.php
@@ -15,6 +15,16 @@ class ExtractEmailRole
      */
     public function handle($request, Closure $next)
     {
+        $email = $request->get('email', null);
+        if (!is_null($email))
+        {
+            $extracted = explode('--', $email);
+            if (count($extracted) >= 2)
+            {
+                $request->merge(['email' => $extracted[0], 'role' => $extracted[1]]);
+            }
+        }
+
         return $next($request);
     }
 }
