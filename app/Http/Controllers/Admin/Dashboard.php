2e338d608e66990c5a1ccd4fda635c1e614c29fe
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-04 19:07:39 +0700

    Initial commit of Admin Dashboard Controller

---
+++ app/Http/Controllers/Admin/Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class Dashboard extends Controller
+{
+    /**
+     * Dashboard constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
