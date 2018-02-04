ef5b9add5c923b20527ed36fcb3fd714a75698d8
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-04 20:04:11 +0700

    Finishing admin dashboard home layout

---
+++ raw/public/js/layout/admin/dashboard/home/admin_dashboard_home_default.js
@@ -0,0 +1,86 @@
+(function ($) {
+    $(function () {
+        Vue.component('list-event', {
+            props: ['event', 'event_id'],
+            template: "<li><a :href=\"this.$parent.home +'/admin/event/' + event_id\">{{event.event}}</a></li>",
+        });
+
+        var app = new Vue({
+            el: '#app',
+            data: {
+                is_process: true,
+                f_event: '',
+                f_slug: '',
+                l_events: {},
+                home: $('meta[name=home]').attr("content"),
+            },
+            methods: {
+                eventFormCommit: function () {
+                    NProgress.configure({parent: '.sweet-modal', showSpinner: false});
+                    NProgress.start();
+                    app.is_process = true;
+
+                    var event    = PojsoMapper.Event(app.f_event, app.f_slug);
+                    var computed = {};
+                    var mapping  = DataMapper.Event(
+                        firebase.auth().currentUser.uid,
+                        firebase.database().ref().child(DataMapper.Event(firebase.auth().currentUser.uid)['events']).push().key);
+                    $.each(mapping, function (key, value) {
+                        switch (key)
+                        {
+                            case 'events' :
+                            {
+                                computed[value]        = Object.assign({}, event);
+                                computed[value]['uid'] = firebase.auth().currentUser.uid;
+                            }
+                                break;
+                            default :
+                            {
+                                computed[value] = Object.assign({}, event);
+                            }
+                                break;
+                        }
+                    });
+
+                    return firebase.database().ref().update(computed, function (error) {
+                        if (error !== undefined)
+                        {
+                            DoNotify(['Pembuatan Event berhasil'])
+                        } else
+                        {
+                            DoNotify([error])
+                        }
+                        NProgress.done();
+                        app.is_process = false;
+                    });
+                },
+                eventFormOpen: function () {
+                    this.$refs.modal.open();
+                },
+                eventListInitial: function () {
+                    var eventsRef = firebase.database().ref(DataMapper.Event(firebase.auth().currentUser.uid)['user']);
+                    eventsRef.on('child_added', function (data) {
+                        Vue.set(app.l_events, data.key, data.val())
+                    });
+                }
+            }
+        });
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user)
+            {
+                console.log(user.email);
+                app.is_process = false;
+                app.eventListInitial();
+            } else
+            {
+                // User is signed out.
+                // ...
+            }
+            document.body.className += 'loaded';
+        });
+    });
+    /*
+     * Run right away
+     * */
+
+})(jQuery);
---
+++ raw/resources/views/layout/admin/dashboard/home/admin_dashboard_home_default.blade.php
@@ -0,0 +1,46 @@
+@extends('root.root-authenticated-theme-default')
+@section('head-title')
+    <title>Dashboard</title>
+@endsection
+
+@section('head-description')
+    <meta name="description" content="Dashboard">
+@endsection
+
+@section('body-content')
+    @parent
+    <div id="app">
+        <sweet-modal ref="modal" title="Buat Event Baru">
+            <input type="text" name="event" placeholder="Event" v-model="f_event" required :disabled="is_process">
+            <br>
+            <input type="text" name="slug" placeholder="Event-ID" v-model="f_slug" required :disabled="is_process">
+            <br>
+            <button slot="button" @click="eventFormCommit" :disabled="is_process">Submit</button>
+        </sweet-modal>
+        <button @click="eventFormOpen">Buat Event Baru</button>
+        <hr>
+        <ol>
+            <list-event
+                    v-for="(value, key) in l_events"
+                    :event="value"
+                    :event_id="key"
+                    :key="key"
+            >
+            </list-event>
+        </ol>
+    </div>
+@endsection
+
+@section('head-css-post')
+    @parent
+    <link rel="stylesheet" href="{{asset('/css/layout/admin/dashboard/home/admin_dashboard_home_default.min.css')}}">
+@endsection
+
+@section('body-js-lower-post')
+    @parent
+    <script type="text/javascript" src="{{asset('/js/layout/admin/dashboard/home/admin_dashboard_home_default.min.js')}}"></script>
+@endsection
+
+@push('additional-firebase-library')
+    <script type="text/javascript" src="{{asset('/vendor/firebase/firebase-database.min.js')}}"></script>
+@endpush
