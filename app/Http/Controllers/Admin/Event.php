263fbf58024993b783506d059c0c83b7c1b5d5e2
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-06 06:29:39 +0700

    Initial Commit of Event Controller

---
+++ app/Http/Controllers/Admin/Event.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class Event extends Controller
+{
+    /**
+     * Dashboard constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
