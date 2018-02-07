a37cf6fd64cc8b313974ba3e0a7ca20d7db8a5f9
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-08 05:48:47 +0700

    Initial commit of registrar dashboard controller

---
+++ app/Http/Controllers/Registrar/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Registrar;
+
+use App\Http\Controllers\Controller;
+
+class Dashboard extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
