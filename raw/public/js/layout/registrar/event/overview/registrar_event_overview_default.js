cb9afe8b9ec519c862659d6e1ec3355c1b793dea
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-08 21:12:12 +0700

    Finishing Event Overview

---
+++ raw/public/js/layout/registrar/event/overview/registrar_event_overview_default.js
@@ -0,0 +1,30 @@
+(function ($) {
+    $(function () {
+        var app = new Vue({
+            el: '#app',
+            data: {
+                home: $('meta[name=home]').attr("content"),
+                event: $('meta[name=event]').attr("content"),
+                role: $('meta[name=user-role]').attr("content"),
+                uid: undefined,
+            },
+            methods: {}
+        });
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user)
+            {
+                app.uid = user.uid;
+                console.log(user.email);
+            } else
+            {
+                // User is signed out.
+                // ...
+            }
+            document.body.className += 'loaded';
+        });
+    });
+    /*
+     * Run right away
+     * */
+
+})(jQuery);
---
+++ raw/resources/views/layout/registrar/event/overview/registrar_event_overview_default.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.admin.event.admin_event_root_default')
+@section('head-title')
+    <title>Overview</title>
+@endsection
+
+@section('head-description')
+    <meta name="description" content="Overview">
+@endsection
+
+@section('body-content')
+    @parent
+    <div id="app">
+
+    </div>
+@endsection
+
+@section('head-css-post')
+    @parent
+    <link rel="stylesheet" href="{{asset('/css/layout/registrar/event/overview/registrar_event_overview_default.min.css')}}">
+@endsection
+
+@section('body-js-lower-post')
+    @parent
+    <script type="text/javascript" src="{{asset('/js/layout/registrar/event/overview/registrar_event_overview_default.min.js')}}"></script>
+@endsection
+
+@push('additional-firebase-library')
+    <script type="text/javascript" src="{{asset('/vendor/firebase/firebase-database.min.js')}}"></script>
+@endpush
