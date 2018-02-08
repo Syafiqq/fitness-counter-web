423d04b4b2f2bfae7aa2341afb1ae5663458933b
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-08 20:56:07 +0700

    Initial commit of registrar event controller

---
+++ app/Http/Controllers/Registrar/Event.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Registrar;
+
+use App\Http\Controllers\Controller;
+
+class Event extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+}
