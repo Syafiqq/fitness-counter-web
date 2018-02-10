1dea35effbf92165c980b0bb50dbc449e333bf5d
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-10 18:52:58 +0700

    Add Preset Model

--- app/Model/PresetModel.php
+++ app/Model/PresetModel.php
@@ -4,7 +4,19 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property \Carbon\Carbon $created_at
+ * @property int $id
+ * @property \Carbon\Carbon $updated_at
+ */
 class PresetModel extends Model
 {
-    //
+    const ID = 'id';
+    const PRESET = 'preset';
+    const PARTICIPANT = 'participant';
+
+    protected $table = 'preset_queue';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    protected $guarded = ['id', 'preset', 'participant'];
 }
