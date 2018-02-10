df973531153a2a61ebe0718939d86fc66b26d7af
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-10 22:10:52 +0700

    Add postQueueAddApi

--- app/Http/Controllers/Registrar/Event.php
+++ app/Http/Controllers/Registrar/Event.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Registrar;
 
+use App\Firebase\PopoMapper;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PresetQueueRequest;
+use App\Model\PresetModel;
+use Illuminate\Database\Query\Builder;
 
 class Event extends Controller
 {
@@ -16,5 +20,24 @@ class Event extends Controller
         $this->meta['event'] = $event;
 
         return view("layout.registrar.event.overview.registrar_event_overview_{$this->theme}", ['meta' => $this->meta]);
+    }
+
+    public function postQueueAddApi(PresetQueueRequest $request)
+    {
+        /** @var PresetModel|Builder $model */
+        $model         = new PresetModel();
+        $currentPreset = $model
+            ->where(PresetModel::PRESET, $request->get('preset', '-'))
+            ->where(PresetModel::PARTICIPANT, $request->get('participant', '-'))
+            ->first();
+        if (is_null($currentPreset))
+        {
+            $model->setAttribute(PresetModel::PRESET, $request->get('preset', '-'));
+            $model->setAttribute(PresetModel::PARTICIPANT, $request->get('participant', '-'));
+            $model->save();
+            $currentPreset = $model;
+        }
+
+        return response()->json(PopoMapper::jsonResponse(200, 'success', ['queue' => $model->where(PresetModel::PRESET, $request->get('preset', '-'))->where(PresetModel::ID, '<=', $currentPreset->getAttribute(PresetModel::ID))->count()]), 200);
     }
 }
