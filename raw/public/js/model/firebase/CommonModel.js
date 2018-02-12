80cee1b42805f7b8879232523244635ca1b3b468
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-12 21:02:41 +0700

    Fix key creation

--- raw/public/js/model/firebase/CommonModel.js
+++ raw/public/js/model/firebase/CommonModel.js
@@ -5,12 +5,11 @@
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
-function createNewEvent(firebase, data)
+function createNewEvent(firebase, data, eventKey = firebase.database().ref().child(DataMapper.Event()['events']).push().key)
 {
-    var eventKey = firebase.database().ref().child(DataMapper.Event()['events']).push().key;
-    var event    = PojsoMapper.Event(data.event, data.slug, firebase.auth().currentUser.uid);
-    var query    = {};
-    var mapping  = DataMapper.Event(
+    var event   = PojsoMapper.Event(data.event, data.slug, firebase.auth().currentUser.uid);
+    var query   = {};
+    var mapping = DataMapper.Event(
         firebase.auth().currentUser.uid,
         data.role,
         eventKey
@@ -26,14 +25,13 @@ function createNewEvent(firebase, data)
     return firebase.database().ref().update(query);
 }
 
-function createNewPreset(firebase, data)
+function createNewPreset(firebase, data, presetKey = firebase.database().ref().child(DataMapper.Preset(null, null)['presets']).push().key)
 {
-    var presetKey = firebase.database().ref().child(DataMapper.Preset(null, null)['presets']).push().key;
-    var query     = {};
-    var mapping   = DataMapper.Preset(
+    var query   = {};
+    var mapping = DataMapper.Preset(
         data.event,
         presetKey);
-    var presets   = PojsoMapper.Preset(data.event, presetKey);
+    var presets = PojsoMapper.Preset(data.event, presetKey);
     _.forEach(mapping, function (value, key) {
         switch (key)
         {// @formatter:off
