eac01bab914934f66d0a3d1a03e8fb37b45a88c3
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-17 21:16:16 +0700

    Finishing registrar event managementi

--- raw/public/js/layout/admin/event/management/registrar/admin_event_management_registrar_default.js
+++ raw/public/js/layout/admin/event/management/registrar/admin_event_management_registrar_default.js
@@ -18,7 +18,21 @@
             },
             methods: {
                 save: function () {
-                    console.log(app.registrar)
+                    NProgress.start();
+                    var query = {};
+                    _.forEach(this.registrar, function (value) {
+                        query[DataMapper.Event(value.uid, 'registrar', app.event)['users']] = value.participate
+                    });
+                    firebase.database().ref().update(query).then(function (error) {
+                        if (error == null)
+                        {
+                            DoNotify(['Perubahan Hak Akses Berhasil'])
+                        } else
+                        {
+                            DoNotify([error])
+                        }
+                        NProgress.done();
+                    });
                 },
                 check: function (uid, participate) {
                     _.filter(app.registrar, {uid: uid})[0].participate = !participate
@@ -29,7 +43,7 @@
         function listRegistrar()
         {
             firebase.database().ref(DataMapper.Users()['users']).orderByChild('roles/registrar').equalTo(true).on("child_added", function (snapshot) {
-                app.registrar.push(PojsoMapper.UserManagement(snapshot.key, snapshot.val()).registrar)
+                app.registrar.push(PojsoMapper.UserManagement(snapshot.key, app.event, 'registrar', snapshot.val()).registrar)
             });
         }
 
