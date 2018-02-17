2958e03720be5c00f35e2cbbe6e248be7813baf3
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-17 17:01:24 +0700

    Add Overview menu

--- raw/resources/views/layout/admin/event/admin_event_root_default.blade.php
+++ raw/resources/views/layout/admin/event/admin_event_root_default.blade.php
@@ -1,5 +1,9 @@
 @extends('root.root-authenticated-theme-default')
 
+@push('pre-add-auth-header-menu')
+    {!! link_to_route(\Illuminate\Support\Facades\Auth::user()->getRole().'.event.overview', 'Overview', [$meta['event']], []) !!}
+@endpush
+
 @section('head-meta')
     @parent
     @include('layout.common.event.event_meta')
