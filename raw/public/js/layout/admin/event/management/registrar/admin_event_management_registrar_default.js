1e476b8937024f238195507649c3e4b112c7985a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-17 19:54:02 +0700

    Finishing user list

--- raw/public/js/layout/admin/event/management/registrar/admin_event_management_registrar_default.js
+++ raw/public/js/layout/admin/event/management/registrar/admin_event_management_registrar_default.js
@@ -6,16 +6,24 @@
                 home: $('meta[name=home]').attr("content"),
                 event: $('meta[name=event]').attr("content"),
                 role: $('meta[name=user-role]').attr("content"),
-                qt_columns: ['name'],
+                qt_columns: ['name', 'participate'],
                 registrar: [],
                 qt_options: {
                     headings: {
                         name: 'Nama',
+                        participate: 'Hak Akses',
                     },
                     sortable: ['queue'],
                 }
             },
-            methods: {}
+            methods: {
+                save: function () {
+                    console.log(app.registrar)
+                },
+                check: function (uid, participate) {
+                    _.filter(app.registrar, {uid: uid})[0].participate = !participate
+                }
+            },
         });
 
         function listRegistrar()
