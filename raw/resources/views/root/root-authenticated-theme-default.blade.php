8a2c4e3d6c1e0af893d200cee8354220c3c38885
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-20 07:02:05 +0700

    - extension -- change to root-firbase-default - head-meta -- remove home - body-js-lower-post -- remove firebase initialization

--- raw/resources/views/root/root-authenticated-theme-default.blade.php
+++ raw/resources/views/root/root-authenticated-theme-default.blade.php
@@ -1,7 +1,6 @@
-@extends('root.root-theme-default')
+@extends('root.root-firebase-default')
 @section('head-meta')
     @parent
-    <meta name="home" content="{!! url('/') !!}">
     @auth
         <meta name="user-role" content="{!! \Illuminate\Support\Facades\Auth::user()->getRole() !!}">
     @endauth
@@ -30,22 +29,7 @@
 @section('body-js-lower-post')
     @parent
     @auth
-        <script type="text/javascript" src="{{asset('/vendor/firebase/firebase-app.min.js')}}"></script>
-        <script type="text/javascript" src="{{asset('/vendor/firebase/firebase-auth.min.js')}}"></script>
-        @stack('additional-firebase-library')
-        <script type="text/javascript" src="{{asset('/js/model/firebase/PathMapper.min.js')}}"></script>
-        <script type="text/javascript" src="{{asset('/js/model/firebase/DataMapper.min.js')}}"></script>
-        <script type="text/javascript" src="{{asset('/js/model/firebase/PojsoMapper.min.js')}}"></script>
-        <script type="text/javascript" src="{{asset('/js/model/firebase/CommonModel.min.js')}}"></script>
         <script type="text/javascript">
-            firebase.initializeApp({
-                apiKey: "AIzaSyD_xXi_xZo25ASGgFODWv9av5lLLPHRWeg",
-                authDomain: "fitness-counter-research.firebaseapp.com",
-                databaseURL: "https://fitness-counter-research.firebaseio.com",
-                projectId: "fitness-counter-research",
-                storageBucket: "fitness-counter-research.appspot.com",
-                messagingSenderId: "691550840999"
-            });
             firebase.auth().signInWithCustomToken("{!! \Illuminate\Support\Facades\Auth::user()->getToken()['token'] !!}").catch(function (error) {
                 console.log(error.code, error.message);
             });
