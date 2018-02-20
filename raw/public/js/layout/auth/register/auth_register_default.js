b8b9ca7797904229287a6ee42c235df1f7599cea
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-20 21:52:27 +0700

    - add -- Completing register flow

--- raw/public/js/layout/auth/register/auth_register_default.js
+++ raw/public/js/layout/auth/register/auth_register_default.js
@@ -12,7 +12,58 @@
             },
             methods: {
                 doRegister: function () {
+                    NProgress.start();
+                    firebase.auth().createUserWithEmailAndPassword(this.f_email, this.f_password).catch(function (error) {
+                        var errorCode    = error.code;
+                        var errorMessage = error.message;
+                        // @formatter:off
+                        switch (errorCode)
+                        {
+                            case 'auth/email-already-in-use' : DoNotify(['Email sudah digunakan']); break;
+                            case 'auth/invalid-email' : DoNotify(['Email tidak valid']); break;
+                            case 'auth/operation-not-allowed' : DoNotify(['Regisrasi gagal dilakukan']); break;
+                            case 'auth/weak-password' : DoNotify(['Password terlalu lemah']); break;
+                            default : DoNotify([errorMessage]); break;
+                        }
+                        // @formatter:on
+                    }).then(function (result) {
+                        if (result != null)
+                        {
+                            firebase.auth().signInWithEmailAndPassword(app.f_email, app.f_password).catch(function (error) {
+                            }).then(function (result) {
+                                var registerRole = function (uid) {
+                                    return new Promise(function (resolve) {
+                                        createNewUser(firebase, {
+                                            role: 'registrar',
+                                            name: app.f_name
+                                        }, uid).then(function (error) {
+                                            if (error != null)
+                                            {
+                                                return registerRole().then(function () {
+                                                    resolve(true);
+                                                })
+                                            }
+                                            else
+                                            {
+                                                resolve(true);
+                                            }
+                                        })
+                                    });
+                                };
 
+                                if (result != null)
+                                {
+                                    registerRole(firebase.auth().currentUser.uid).then(function (result) {
+                                        firebase.auth().signOut().then(function (result) {
+                                        }).catch(function (error) {
+                                        });
+                                    });
+                                }
+                            });
+                            DoNotify(['Pendaftaran Berhasil']);
+                        }
+                        NProgress.done();
+                    });
                 }
             }
         });
