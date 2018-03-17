4c55b5fcc7266ad4f2418ae7de2bcf8286bfe33f
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-18 05:55:24 +0700

    Add participant getter

--- raw/public/js/layout/admin/event/report/evaluation/admin_event_report_evaluation_default.js
+++ raw/public/js/layout/admin/event/report/evaluation/admin_event_report_evaluation_default.js
@@ -6,15 +6,42 @@
                 home: $('meta[name=home]').attr("content"),
                 event: $('meta[name=event]').attr("content"),
                 role: $('meta[name=user-role]').attr("content"),
-                qt_columns: ['name', 'participate'],
+                vault: {},
+                qt_columns: ['queue', 'no', 'name', 'gender', 'same'],
+                queues: [],
+                qt_options: {
+                    headings: {
+                        queue: 'Antrian',
+                        no: 'Nomor',
+                        name: 'Nama',
+                        gender: 'Jenis Kelamin',
+                        same: 'Kemiripan',
+                    },
+                    sortable: ['queue', 'no', 'name', 'gender', 'same'],
+                }
             },
-            methods: {},
         });
+
+        function listParticipant(event)
+        {
+            firebase.database().ref(DataMapper.Event(null, null, event)['events'] + '/preset_active').once('value').then(function (preset) {
+                firebase.database().ref(DataMapper.PresetQueue(preset.val())['presets']).on('child_added', function (datedQueue) {
+                    app.vault[datedQueue.key] = {};
+                    _.forEach(datedQueue.val(), function (queue) {
+                        if (queue != null && queue.participant.queue != null)
+                        {
+                            app.vault[datedQueue.key][queue.participant.queue] = queue;
+                        }
+                    });
+                });
+            });
+        }
 
         firebase.auth().onAuthStateChanged(function (user) {
             if (user)
             {
                 console.log(user.email);
+                listParticipant(app.event);
             } else
             {
                 // User is signed out.
