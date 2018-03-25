740b94f69d01ef869affe7954dc0a7643b043724
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-25 13:14:55 +0700

    Initial Commit of Auth login bulma

---
+++ raw/resources/views/layout/auth/login/auth_login_bulma.blade.php
@@ -0,0 +1,35 @@
+@extends('root.root-theme-default')
+<?php
+/** @var \Collective\Html\FormBuilder $form */
+$form = \Collective\Html\FormFacade::getFacadeRoot();
+?>
+@section('head-title')
+    <title>Login</title>
+@endsection
+
+@section('head-description')
+    <meta name="description" content="Login">
+@endsection
+
+@section('body-content')
+    @parent
+    {{--{!! $form->open(['route' => 'auth.login.post', 'method' => 'post']) !!}
+    {!! nl2br(PHP_EOL) !!}
+    {!! $form->email('email', 'syafiq.rezpector@gmail.com', ['placeholder' => 'Email', 'required'=> true]) !!}
+    {!! nl2br(PHP_EOL) !!}
+    {!! $form->input('password', 'password', 'password', ['value' => 'password','placeholder' => 'Password', 'required'=> true]) !!}
+    {!! nl2br(PHP_EOL) !!}
+    {!! $form->button('Submit', ['type' => 'Submit']) !!}
+    {!! $form->close() !!}--}}
+@endsection
+
+@section('head-css-post')
+    @parent
+    <link rel="stylesheet" href="{{asset('/css/layout/auth/login/auth_login_bulma.min.css')}}">
+@endsection
+
+@section('body-js-lower-post')
+    @parent
+    <script type="text/javascript" src="{{asset('js/layout/auth/login/auth_login_bulma.min.js')}}"></script>
+@endsection
+
