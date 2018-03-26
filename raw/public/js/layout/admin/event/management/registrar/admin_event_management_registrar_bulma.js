798a8c9eb5019fe6adfba8aaa447755ea54bec26
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-26 23:47:02 +0700

    Phase 1 of admin event management registrar

---
+++ raw/public/js/layout/admin/event/management/registrar/admin_event_management_registrar_bulma.js
@@ -0,0 +1,67 @@
+(function ($) {
+    $(function () {
+        var app = new Vue({
+            el: '#app',
+            data: {
+                home: $('meta[name=home]').attr("content"),
+                event: $('meta[name=event]').attr("content"),
+                role: $('meta[name=user-role]').attr("content"),
+                qt_columns: ['name', 'participate'],
+                registrar: [],
+                qt_options: {
+                    headings: {
+                        name: 'Nama',
+                        participate: 'Hak Akses',
+                    },
+                    sortable: ['queue'],
+                }
+            },
+            methods: {
+                save: function () {
+                    NProgress.start();
+                    var query = {};
+                    _.forEach(this.registrar, function (value) {
+                        query[DataMapper.Event(value.uid, 'registrar', app.event)['users']] = value.participate === false ? null : value.participate
+                    });
+                    firebase.database().ref().update(query).then(function (error) {
+                        if (error == null)
+                        {
+                            DoNotify(['Perubahan Hak Akses Berhasil'])
+                        } else
+                        {
+                            DoNotify([error])
+                        }
+                        NProgress.done();
+                    });
+                },
+                check: function (uid, participate) {
+                    _.filter(app.registrar, {uid: uid})[0].participate = !participate
+                }
+            },
+        });
+
+        function listRegistrar()
+        {
+            firebase.database().ref(DataMapper.Users()['users']).orderByChild('roles/registrar').equalTo(true).on("child_added", function (snapshot) {
+                app.registrar.push(PojsoMapper.UserManagement(snapshot.key, app.event, 'registrar', snapshot.val()).role)
+            });
+        }
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user)
+            {
+                console.log(user.email);
+                listRegistrar();
+            } else
+            {
+                // User is signed out.
+                // ...
+            }
+            removeCurtain();
+        });
+    });
+    /*
+     * Run right away
+     * */
+
+})(jQuery);
