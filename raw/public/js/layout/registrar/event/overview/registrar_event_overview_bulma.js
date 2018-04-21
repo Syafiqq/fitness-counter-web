d19041096324cfcd65a7732dd9f605dac26f9163
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-04-21 16:10:34 +0700

    Change to original burger

--- raw/public/js/layout/registrar/event/overview/registrar_event_overview_bulma.js
+++ raw/public/js/layout/registrar/event/overview/registrar_event_overview_bulma.js
@@ -178,7 +178,7 @@
             }
             removeCurtain();
         });
-        var burger = document.querySelector('#burger-new');
+        var burger = document.querySelector('#burger');
         var menu   = document.querySelector('#' + burger.dataset.target);
         burger.addEventListener('click', function () {
             burger.classList.toggle('is-active');
