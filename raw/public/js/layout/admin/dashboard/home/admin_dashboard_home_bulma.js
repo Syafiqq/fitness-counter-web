d2fa4ddda4b7903e4c7a0fd3f48008ef5a4828e1
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-04-21 16:10:52 +0700

    Remove unnecessary burger

--- raw/public/js/layout/admin/dashboard/home/admin_dashboard_home_bulma.js
+++ raw/public/js/layout/admin/dashboard/home/admin_dashboard_home_bulma.js
@@ -87,10 +87,4 @@
     /*
      * Run right away
      * */
-    var burger = document.querySelector('.burger');
-    var menu   = document.querySelector('#' + burger.dataset.target);
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
 })(jQuery);
