151b7226db9c744c47a38ee7b84203716f68e21c
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-04-21 16:36:33 +0700

    accept blob data

--- raw/public/js/layout/admin/event/report/evaluation/admin_event_report_evaluation_bulma.js
+++ raw/public/js/layout/admin/event/report/evaluation/admin_event_report_evaluation_bulma.js
@@ -150,8 +150,8 @@
                     this.processed['oVal'] = app.vault[aVal['pdk']][aVal['pqu']];
                     filterEdit(this.processed['oVal'], this.processed['pVal']);
                     if (_.filter(this.processed.pVal, function (o) {
-                            return o.show;
-                        }).length > 0)
+                        return o.show;
+                    }).length > 0)
                     {
                         this.$modal.show('editable-modal');
                     }
@@ -210,6 +210,7 @@
                                     participant: 3,
                                 }
                                 , {
+                                    responseType: 'blob',
                                     headers: {
                                         'Accept': 'application/json',
                                         'Content-Type': 'application/json;charset=UTF-8',
@@ -219,12 +220,10 @@
                                 .then(function (response) {
                                     console.log(response);
                                     that.$swal.close();
-                                    var $a = $("<a>");
-                                    $a.attr("href", response['data']['data']['download']['content']);
-                                    $("body").append($a);
-                                    $a.attr("download", response['data']['data']['download']['filename']);
-                                    $a[0].click();
-                                    $a.remove();
+                                    if ('data' in response && 'headers' in response && 'content-disposition' in response.headers && 'content-type' in response.headers)
+                                    {
+                                        fileDownload(response.data, getFilename(response.headers['content-disposition'], response.headers['content-type']));
+                                    }
                                     NProgress.done();
 
                                 })
