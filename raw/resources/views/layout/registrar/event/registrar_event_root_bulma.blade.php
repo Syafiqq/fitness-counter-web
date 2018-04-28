849874a8312e459ebf6993ddf1330d4b8c3c346a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-04-28 07:58:22 +0700

    Add js

--- raw/resources/views/layout/registrar/event/registrar_event_root_bulma.blade.php
+++ raw/resources/views/layout/registrar/event/registrar_event_root_bulma.blade.php
@@ -4,3 +4,9 @@
     @parent
     @include('layout.common.event.event_meta')
 @endsection
+@section('body-js-lower-post')
+    @parent
+    @auth
+        <script type="text/javascript" src="{{asset('/js/layout/registrar/event/registrar_event_root_bulma.min.js')}}"></script>
+    @endauth
+@endsection
