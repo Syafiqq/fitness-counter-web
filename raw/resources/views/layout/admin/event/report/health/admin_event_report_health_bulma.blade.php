dae00eb0fb2b57a0aca33864bba3534398e798e4
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-04-28 12:41:39 +0700

    Fix awkward fixed table height

--- raw/resources/views/layout/admin/event/report/health/admin_event_report_health_bulma.blade.php
+++ raw/resources/views/layout/admin/event/report/health/admin_event_report_health_bulma.blade.php
@@ -444,7 +444,7 @@
                                 Laporan Evaluasi
                             </p>
                         </header>
-                        <div class="card-table is-fullheight" style="height: 900px; max-height: 900px!important;">
+                        <div class="card-table is-fullheight">
                             <div class="content" style="margin: 20px">
                                 <v-client-table :data="queues" :columns="qt_columns" :options="qt_options">
                                     <div slot="action" slot-scope="props">
