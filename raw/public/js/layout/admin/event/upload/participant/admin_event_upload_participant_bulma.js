18c173e2b87f96609a2e714545c2566b1109bdc1
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-04-28 08:46:10 +0700

    Phase 1 Adding Script

---
+++ raw/public/js/layout/admin/event/upload/participant/admin_event_upload_participant_bulma.js
@@ -0,0 +1,58 @@
+(function ($) {
+    $(function () {
+        moment.locale('id');
+        var app = new Vue({
+            el: '#app',
+            data: {
+                home: $('meta[name=home]').attr("content"),
+                event: $('meta[name=event]').attr("content"),
+                role: $('meta[name=user-role]').attr("content"),
+                preset: undefined,
+                is_process: true,
+                queues: [],
+                qt_columns: ['no', 'name', 'gender'],
+                qt_options: {
+                    uniqueKey: 'no',
+                    headings: {
+                        no: 'No SBMPTN',
+                        name: 'Nama',
+                        gender: 'Jenis Kelamin',
+                    },
+                    sortable: ['no', 'name', 'gender'],
+                }
+            },
+            computed: {},
+            methods: {
+                downloadTemplate: function () {
+
+                },
+                uploadParticipant: function () {
+
+                }
+            },
+
+        });
+
+        function listParticipant(event)
+        {
+
+        }
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user)
+            {
+                console.log(user.email);
+                //listParticipant(app.event);
+            } else
+            {
+                // User is signed out.
+                // ...
+            }
+            removeCurtain();
+        });
+    });
+    /*
+     * Run right away
+     * */
+
+})(jQuery);
